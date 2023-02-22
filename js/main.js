03808c0b3c48061b662aa7af196f3a88aa18b601
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-22 15:19:36 +0100

    Final

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
 const tariffaKm = 0.21;
-let msg;
-
-const inputPercorrenza = parseInt(prompt('Inserire i KM: '));
+const inputPercorrenza = parseInt(prompt('Inserire la percorrenza (km): '));
 const inputEta = parseInt(prompt("Inserire l'età del passeggero: ")) ;
+
+let msg;
+let prezzo;
 
 
 if (isNaN(inputPercorrenza) || isNaN(inputEta)){
+    msg = "ERRORE: Valori in input non numerici.";
     console.log("ERRORE: Valori in input non numerici.");
+} else {
+    prezzo = inputPercorrenza * tariffaKm;
 }
 
 
-let prezzo = inputPercorrenza * tariffaKm;
-
 if (inputEta < 18){
 
-    msg = 'Hai diritto allo sconto under 18!';
+    msg = 'Buone notizie: hai diritto allo sconto under 18!';
     prezzo -= (prezzo * 0.2); 
 
 } else if (inputEta >= 65) {
 
-    msg = 'Hai diritto allo sconto senior!';
+    msg = 'Buone notizie: hai diritto allo sconto senior!';
     prezzo -= (prezzo * 0.4); 
 }
 
+if(prezzo > 0){
+    prezzo = prezzo.toFixed(2);
+    document.getElementById('priceBox').innerHTML = prezzo;
+}
 
-console.log(prezzo);
+if (msg != null){
+    document.getElementById('msgBox').innerHTML = msg;
+}
+
+
