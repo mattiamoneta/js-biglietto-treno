9a03bf71dd37e58edd062b20160adf3bd1f99dbf
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-22 15:25:36 +0100

    Final

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const inputEta = parseInt(prompt("Inserire l'età del passeggero: ")) ;
 let msg;
 let prezzo;
 
-
+// Verifica se valore è numerico
 if (isNaN(inputPercorrenza) || isNaN(inputEta)){
     msg = "ERRORE: Valori in input non numerici.";
     console.log("ERRORE: Valori in input non numerici.");
@@ -14,19 +14,26 @@ if (isNaN(inputPercorrenza) || isNaN(inputEta)){
 }
 
 
-if (inputEta < 18){
+// Verifica se valore in input non è negativo
+// e se sia applicabile lo sconto
+if ((inputEta < 18) && (inputEta > 0) && (inputPercorrenza > 0)){
 
     msg = 'Buone notizie: hai diritto allo sconto under 18!';
     prezzo -= (prezzo * 0.2); 
 
-} else if (inputEta >= 65) {
+} else if ((inputEta >= 65) && (inputPercorrenza > 0)) {
 
     msg = 'Buone notizie: hai diritto allo sconto senior!';
     prezzo -= (prezzo * 0.4); 
+
+} else {
+    msg = "ERRORE: L'età e la percorrenza non possono essere zero.";
 }
 
+
+// Arrotonda il prezzo a due decimali
 if(prezzo > 0){
-    prezzo = prezzo.toFixed(2);
+    prezzo = parseFloat(prezzo.toFixed(2));
     document.getElementById('priceBox').innerHTML = prezzo;
 }
 
